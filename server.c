6bcf2e55e6b8f0c0b69f92bcef4e8c72829b0df8
Binit Koirala <binit.koirala@bison.howard.edu>
2018-10-10 03:27:18 -0400

    Parse terminal parameter and declare variables

--- server.c
+++ server.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>		/* socket() bind() connect() for the server */
+#include <arpa/inet.h> 
+#include <stdlib.h>
+#include <unistd.h>
+#define MAX_REQ 5
+
 
 int main(int argc, char **argv)
 {
-	for (int i = 0; i < argc; ++i)
-	{
-		printf("argv[%d] is %s\n ", i, argv[i]);
+	int server_socket;	/*	socket for server 	*/
+	int client_socket;	/* socket for client */
+	struct sockaddr_in server_address;	/*Server address*/
+	struct sockaddr_in client_address;	/*Client address*/
+	unsigned short server_port;	/* port for server */
+	unsigned int length_of_client_addr;	/*client address length*/
+
+	if (argc != 2){		/* servername port, 2 arguments) */
+		printf("%s <Port>\n",argv[0]);
+		exit(1)
 	}
+
+	server_port = atoi(argv[1]);
+
+
+
+
+
+
+
+
 }
