149605b27626a7b92ae7fc7392deb08154be9a63
Binit Koirala <binit.koirala@bison.howard.edu>
2018-10-10 06:51:55 -0400

    add helper functionsand headers

---
+++ helper.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "helper.h"
+
+
+void ShowError(char *err_message){
+	printf("%s\n",err_message);
+	exit(1);
+	
+}
---
+++ helper.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ShowError(char *err_message);
